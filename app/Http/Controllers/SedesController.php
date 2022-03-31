bffd88f51e5235d795d609d2c016e258bdae0501
Luis Núñez <njluisangel@gmail.com>
2022-03-31 10:38:09 -0600

    Listado y Actualización de Sedes

--- app/Http/Controllers/SedesController.php
+++ app/Http/Controllers/SedesController.php
@@ -13,7 +13,14 @@ class SedesController extends Controller
     //Listado de sedes
     public function index(){
 
-        return 'Listado de Sedes';
+        $sedes = Sedes::all();
+
+        $json = array(
+            "status" => 404,
+            "detalles" => $sedes,
+        );
+
+        return json_encode($json, true);
 
     }
 
@@ -123,6 +130,115 @@ class SedesController extends Controller
                 "details" => "Error al eliminar la sede solicitada."
             );
             return json_encode($json, true);
+        }
+
+    }
+
+    //Actualización de una sede
+    public function update($id_sede, Request $request){
+
+        //Listamos si existe el ID solicitado
+        $sede = DB::table('sedes')
+            ->where('sedes.id_sede', $id_sede)
+            ->get();
+
+        //En caso de que sí exista continuamos con la edición del usuario
+        if(!empty($sede[0])){
+
+            //Recolección de datos de los input
+            $datos = array( "id_municipio"=>$request->input('id_municipio'),
+                "nombre"=>$request->input('nombre'),
+                "direccion"=>$request->input('direccion'),
+                "cruce_calles"=>$request->input('cruce_calles'),
+                "colonia"=>$request->input('colonia'),
+                "cp"=>$request->input('cp'),
+                "latitud"=>$request->input('latitud'),
+                "longitud"=>$request->input('longitud'),
+                "georeferencia"=>$request->input('georeferencia'),
+                "nombre_encargado"=>$request->input('nombre_encargado'),
+                "tel_encargado"=>$request->input('tel_encargado'),
+                "email_encargado"=>$request->input('email_encargado'),
+                "cupo"=>$request->input('cupo'),
+            );
+
+            //Validar formato de datos
+            $validator = Validator::make($datos, [
+                'id_municipio' => 'required|integer',
+                'nombre'=> 'required|string',
+                'direccion'=>'required|string',
+                'cruce_calles'=>'required|string',
+                'colonia'=>'required|string',
+                'cp'=>'required|integer',
+                'latitud'=>'string',
+                'longitud'=>'string',
+                'georeferencia'=>'string',
+                'nombre_encargado'=>'required|string',
+                'tel_encargado'=>'required|integer',
+                'email_encargado'=>'required|email:rfc',
+                'cupo'=>'required|integer',
+            ]);
+
+            //Si falla la validación del formato
+            if ($validator -> fails()) {
+
+                $errors = $validator->errors();
+
+                $json = array(
+                    "status" => 404,
+                    "detalles" => $errors
+                );
+
+                return json_encode($json, true);
+
+            }else{
+
+                $datos = array( "id_municipio"=>$datos['id_municipio'],
+                    "nombre"=>$datos['nombre'],
+                    "direccion"=>$datos['direccion'],
+                    "cruce_calles"=>$datos['cruce_calles'],
+                    "colonia"=>$datos['colonia'],
+                    "cp"=>$datos['cp'],
+                    "latitud"=>$datos['latitud'],
+                    "longitud"=>$datos['longitud'],
+                    "georeferencia"=>$datos['georeferencia'],
+                    "nombre_encargado"=>$datos['nombre_encargado'],
+                    "tel_encargado"=>$datos['tel_encargado'],
+                    "email_encargado"=>$datos['email_encargado'],
+                    "cupo"=>$datos['cupo'],
+                );
+
+                //Actualizamos el registro
+                if ($sede = Sedes::where('id_sede', $id_sede) -> update($datos)){
+
+                    $json = array(
+                        "status" => 200,
+                        "detalles" => "Registro actualizado exitosamente"
+                    );
+
+                    return json_encode($json, true);
+
+                }else{
+
+                    $json = array(
+                        "status" => 404,
+                        "detalles" => "Error al actualizar el registro."
+                    );
+
+                    return json_encode($json, true);
+
+                }
+
+            }
+
+        }else{
+
+            $json = array(
+                "status" => 404,
+                "details" => "No hay ninguna sede registrada con ese Id"
+            );
+
+            return json_encode($json, true);
+
         }
 
     }
